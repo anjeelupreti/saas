46d27da20af00704db59723bbfd7aeb209bbb9dd
anjeelupreti <upretianjeel@gmail.com>
2025-02-12 15:06:02 +0545

    prepare for railway

--- src/core/views.py
+++ src/core/views.py
@@ -7,14 +7,14 @@ from visits.models import PageVisit
 
 def home_view(request , *args, **kwargs):
     total_website_visits=PageVisit.objects.all().count()
-    page_visit=PageVisit.objects.filter(path=request.path).count()
+    page_visits=PageVisit.objects.filter(path=request.path).count()
     try:
-        percent:(page_visit.count()*100.0)/total_website_visits
+        percent=(page_visits*100.0)/total_website_visits
     except:
         percent=0
     context={
         # "page_title":title,
-        "page_visit":page_visit,
+        "page_visit":page_visits,
         "percent_visit":percent,
         "total_website_visits":total_website_visits,
 
